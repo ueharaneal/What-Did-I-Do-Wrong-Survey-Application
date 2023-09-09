0a4a31be777972b609bf1c14631202898f0bcaf9
Neal Uehara <ueharaneal@gmail.com>
2023-09-09 05:44:21 -0700

    tailwind test

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Landing =() =>{
     return(
-        <div style ={{ textAlign: 'center'}}>
+        //<div style ={{ textAlign: 'center'}}>
+        <div className="text-center text-align text-red-500">
             <h1>WDIDW</h1>
              collect feedback from your users. 
         </div>
+        
     )
 }
 
