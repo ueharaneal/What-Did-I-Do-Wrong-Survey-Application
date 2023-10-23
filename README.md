d83e6b9aaa274ae8854f1e852ae5c509d1d058d1
Neal Uehara <ueharaneal@gmail.com>
2023-10-23 03:29:03 -0700

    added all files to new repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# what-did-i-do-wrong
+Live Website View: 
+https://hidden-ridge-03010-2e2246d75af4.herokuapp.com/
+
+This application will allow bossiness or individuals to send customized survey to an email list.
+
+There is two server concurrently running the node and the react server.
+
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Landing = () => {
+    const containerStyle = {
+      textAlign: 'center',
+      padding: '20px',
+    };
+  
+    const headingStyle = {
+      fontSize: '36px',
+      fontWeight: 'bold',
+      marginBottom: '20px',
+    };
+  
+    const paragraphStyle = {
+      fontSize: '18px',
+      lineHeight: '1.5',
+      marginBottom: '10px',
+    };
+  
+    return (
+      <div style={containerStyle}>
+        <h1 style={headingStyle}>Welcome to WDIDW</h1>
+        <p style={paragraphStyle}>Collect valuable feedback from your users.</p>
+        <h2 style={headingStyle}>About</h2>
+        <p style={paragraphStyle}>
+          WDIDW is a platform for gathering feedback to help you improve your products and services.
+        </p>
+        <h2 style={headingStyle}>Quick Start</h2>
+        <p style={paragraphStyle}>Get started in three easy steps:</p>
+        <ol style={{ textAlign: 'left', margin: '0 auto', maxWidth: '400px' }}>
+          <li style={paragraphStyle}>Sign up for an account.</li>
+          <li style={paragraphStyle}>Create a survey.</li>
+          <li style={paragraphStyle}>Collect and analyze feedback.</li>
+        </ol>
+        <h2 style={headingStyle}>How I Created It</h2>
+        <p style={paragraphStyle}>
+          Learn about the technology and tools used to build WDIDW.
+        </p>
+        <div className="fixed-action-btn">
+          <Link to="/surveys/new" className="btn-floating btn-large red">
+            <i className="material-icons">add</i>
+          </Link>
+        </div>
+      </div>
+    );
+  };
+
+export default Landing;
