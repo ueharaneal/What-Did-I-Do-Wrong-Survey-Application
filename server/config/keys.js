386c80e1371b820b07ef83ee01a034169493bf1c
Neal Uehara <ueharaneal@gmail.com>
2023-10-27 06:52:59 -0700

    Final dage commit still need to add dev.js

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-//Figure out what kind of credentials to return
-if(process.env.NODE_ENV === 'production'){
-   //We are in production return production set of keys
-   module.exports = require('./prod');
-}else{
-   //we are in development we are production set of keys
-  module.exports = require('./dev');
-}
--- server/middlewares/requireCredits.js
+++
@@ -1,8 +0,0 @@
-//Checking to see if the user has enough credits. 
-
-module.exports = (req, res, next) => {
-    if(req.user.credits < 1 ){
-        return res.status(403).send({ error: 'Not enough credits'});
-    }
-    next();
-};
