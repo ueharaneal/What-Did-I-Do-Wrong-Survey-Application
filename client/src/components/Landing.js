0ce84c68d8b1bc0f9a76be168dc2bbb81dea82a6
Neal Uehara <ueharaneal@gmail.com>
2023-10-27 02:31:03 -0700

    hope it works

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -17,6 +17,7 @@ const Landing = () => {
       fontSize: '18px',
       lineHeight: '1.5',
       marginBottom: '10px',
+      textAlign: 'left',
     };
   
     return (
@@ -28,47 +29,48 @@ const Landing = () => {
           WDIDW is a platform for gathering feedback to help you improve your products and services.
         </p>
         <h2 style={headingStyle}>Quick Start</h2>
-        <p style={paragraphStyle}>Get started in three easy steps:</p>
+        <p style={paragraphStyle}>Get started in five easy steps:</p>
         <ol style={{ textAlign: 'left', margin: '0 auto', maxWidth: '400px' }}>
           <li style={paragraphStyle}>Sign up for an account.</li>
+          <li style={paragraphStyle}>Click the red button on bottom right corner</li>
           <li style={paragraphStyle}>Buy/add credits (refer to readMe.md)</li>
           <li style={paragraphStyle}>Create a survey and add email recipients.</li>
           <li style={paragraphStyle}>Collect and analyze feedback.</li>
         </ol>
-        <h2 style={headingStyle}>How I Created It</h2>
+        <h2 style={headingStyle}>Technologies Used</h2>
+       
         <p style={paragraphStyle}>
-          React: To create the front-end/client side.
+          <strong>React:</strong> To create the front-end/client side.
         </p>
         <p style={paragraphStyle}>
-          Redux: A state management library for handling application state.
-        </p>
-        
-        <p style={paragraphStyle}>
-          MongoDB: A NoSQL database used for storing and managing email recipients, surveys, and survey responses.
+          <strong>Redux:</strong> A state management library for handling application state.
         </p>
         <p style={paragraphStyle}>
-          Express: Used to help build the server side of the application.
+          <strong>MongoDB:</strong> A NoSQL database used for storing and managing email recipients, surveys, and survey responses.
         </p>
         <p style={paragraphStyle}>
-          Node.js: A JavaScript runtime environment for server-side development.
+          <strong>Express:</strong> Used to help build the server side of the application.
         </p>
         <p style={paragraphStyle}>
-          Materialize: A responsive front-end framework to style this web application.
+          <strong>Node.js:</strong> A JavaScript runtime environment for server-side development.
         </p>
         <p style={paragraphStyle}>
-          Passport.js: Authentication middleware for Node.js.
+          <strong>Materialize:</strong> A responsive front-end framework to style this web application.
         </p>
         <p style={paragraphStyle}>
-          Heroku: A cloud platform used to deploy and host the web application.
+          <strong>Passport.js:</strong> Authentication middleware for Node.js.
         </p>
         <p style={paragraphStyle}>
-          Google OAuth: OAuth 2.0 authentication for Google users.
+          <strong>Heroku:</strong> A cloud platform used to deploy and host the web application.
         </p>
         <p style={paragraphStyle}>
-          Stripe: A payment processing platform for online transactions.
+          <strong>Google OAuth:</strong> OAuth 2.0 authentication for Google users.
         </p>
         <p style={paragraphStyle}>
-          SendGrid: A cloud-based email delivery service used to send survey emails.
+          <strong>Stripe:</strong> A payment processing platform for online transactions.
+        </p>
+        <p style={paragraphStyle}>
+          <strong>SendGrid:</strong> A cloud-based email delivery service used to send survey emails.
         </p>
         
         <div className="fixed-action-btn">
